9c937647db234200f0da3d63b38e8b935dfa2fca
Gary <gary@siestasoft.mygbiz.com>
2013-01-07 19:37:02 -0800

    Added ItemModels

---
+++ website/Models/ItemModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Security;
+
+namespace website.Models
+{
+    [Table("Item")]
+    public class Item
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int ItemId { get; set; }
+
+        [StringLength(256)]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+    }
+
+}
